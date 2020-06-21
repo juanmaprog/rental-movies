565a160e7a565772c01780aec6fd8fd1bb56d3dc
juanmaprog <juanma.prog@gmail.com>
2020-06-21 23:10:14 +0200

    Actualizadas las dependendencias.

--- src/connection.js
+++ src/connection.js
@@ -1,43 +1,10 @@
-// Instalación
-// npm install mongodb --save
-// npm install mongosse
-
-// Cargar films y generar la tabla en la db sin tener que entrar en la propia db.
-// Traer el modelo
-// const FilmModel = require("./models/films");
-// const UserModel = require("./models/users");
-
-// function isConnection(){
-
-//   return false;
-
-// }
-
-// const ConnMongo = {
-//   isConnection() {
-//     return "true";
-//   },
-// };
-
-// module.exports = {
-//   ConnMongo,
-// };
-
-// Exportar para utilizarlo fuera
-// module.exports = {
-//   Film,
-//   User,
-// };
-
 const mongoose = require("mongoose");
 
-// const urlMongo =
-//   "mongodb+srv://<username>:<password>@nordicdb-hvkd5.mongodb.net/test"; //"mongodb://localhost:27017";
-
-// const uri = "mongodb://localhost:27017/RentalMovies";
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log("MONGODB_URI:", MONGODB_URI);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -47,7 +14,7 @@ mongoose
 
 //once execute only one
 mongoose.connection.once("open", (_) => {
-  console.log("Database is connected to", uri);
+  console.log("Database is connected to", MONGODB_URI);
 });
 
 //Emitted if an error occurs on a connection, like a parseError due to malformed data or a payload larger than 16MB
